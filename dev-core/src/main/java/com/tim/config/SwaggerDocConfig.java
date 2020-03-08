daecf21842acf044cdeb87fedaa2a1f576ad3e22
yangtzeyufei <yangtze_yufei@163.com>
2020-03-08 16:17:40 +0800

    feat(pom):父工程统一编译

--- dev-core/src/main/java/com/tim/config/SwaggerDocConfig.java
+++ dev-core/src/main/java/com/tim/config/SwaggerDocConfig.java
@@ -1,5 +1,7 @@
 package com.tim.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +9,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -33,14 +39,52 @@ public class SwaggerDocConfig {
   @Value("${swagger.version}")
   private String swaggerVersion;
 
+  // TODO
+  // 不需要token接口：POST /api/v1/auth/account/login
+  //
+  //所有包含"auth"的接口不需要使用securitySchemes
+  private String swaggerSecurityPath = "^(?!auth).*$";
+
   @Bean
   public Docket createRestApi() {
-    return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-        .apis(RequestHandlerSelectors.basePackage(swaggerBasePackage)).paths(PathSelectors.any())
-        .build();
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo()).select()
+        .apis(RequestHandlerSelectors.basePackage(swaggerBasePackage))
+        .paths(PathSelectors.any())
+        .build()
+        .securitySchemes(securitySchemes())
+        .securityContexts(securityContexts());
   }
 
   private ApiInfo apiInfo() {
     return new ApiInfoBuilder().title(swaggerTitle).version(swaggerVersion).build();
+  }
+
+  private List<ApiKey> securitySchemes() {
+    List apiKeys = new ArrayList<ApiKey>(1);
+    apiKeys.add(new ApiKey("Authorization", "Authorization", "header"));
+
+    return apiKeys;
+  }
+
+  private List<SecurityContext> securityContexts() {
+    List securityContexts = new ArrayList<SecurityContext>(1);
+    securityContexts.add(SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.regex(swaggerSecurityPath))
+        .build());
+
+    return securityContexts;
+  }
+
+  private List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+
+    List securityReferences = new ArrayList<SecurityReference>(1);
+    securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+
+    return securityReferences;
   }
 }
